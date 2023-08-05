9e9c8f5903149eb2906308fc0528f980ee8ab929
KingBBQ <gw@communitylabs.de>
2023-08-05 16:36:41 +0200

    Update code.py

--- code.py
+++ code.py
@@ -13,11 +13,11 @@ layout = KeyboardLayout(keyboard)
 
 
 # workaround: diffirent local keyboard layout...
-import conversion_dvorak
+# import conversion_dvorak
 
 
 def typeIt(string):
-        string = conversion_dvorak.convert_ascii_dvorak(string)
+        #string = conversion_dvorak.convert_ascii_dvorak(string)
         #string += "\n"
         layout.write(string)
 
@@ -26,6 +26,8 @@ debug = False
 
 bnt1_pin = board.GP15
 bnt2_pin = board.GP14
+bnt3_pin = board.GP13
+bnt4_pin = board.GP12
 
 btn1 = digitalio.DigitalInOut(bnt1_pin)
 btn1.direction = digitalio.Direction.INPUT
@@ -34,6 +36,14 @@ btn1.pull = digitalio.Pull.DOWN
 btn2= digitalio.DigitalInOut(bnt2_pin)
 btn2.direction = digitalio.Direction.INPUT
 btn2.pull = digitalio.Pull.DOWN
+
+btn3 = digitalio.DigitalInOut(bnt3_pin)
+btn3.direction = digitalio.Direction.INPUT
+btn3.pull = digitalio.Pull.DOWN
+
+btn4 = digitalio.DigitalInOut(bnt4_pin)
+btn4.direction = digitalio.Direction.INPUT
+btn4.pull = digitalio.Pull.DOWN
 
 
 while True:
@@ -50,11 +60,19 @@ while True:
         keyboard.send(Keycode.SHIFT, Keycode.SEVEN)
         time.sleep(0.5)
         typeIt("gamemode 2\n")
+    elif btn3.value:
+        if debug:
+                print("buttn! 3!")
+        keyboard.send(Keycode.SHIFT, Keycode.SEVEN)
+        time.sleep(0.5)
+        typeIt("gamemode 3\n")
+    elif btn4.value:
+        if debug:
+                print("buttn! 4!")
+        keyboard.send(Keycode.SHIFT, Keycode.SEVEN)
+        time.sleep(0.5)
+        typeIt("gamemode 4\n")
     else:
         if debug:
                 print(".")
     time.sleep(0.1)
-
-
-
-
